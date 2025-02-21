06420fffe3e33dceeb694c883fd614edbe78d606
Lucius Kwok <lk@felttip.com>
2025-02-20 22:41:44 -0500

    Make a circle using the script

--- OMS/Assets/Scripts/OrbitLineScript.cs
+++ OMS/Assets/Scripts/OrbitLineScript.cs
@@ -1,16 +1,34 @@
-using UnityEngine;
+﻿using UnityEngine;
+using System.Collections;
+
 
 public class OrbitLineScript : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+	//public LineRenderer lineRenderer;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	void Start()
+	{
+		// Create an ellipse
+		LineRenderer lineRenderer = GetComponent<LineRenderer>();
+		int count = 64;
+		lineRenderer.positionCount = count;
+
+		var points = new Vector3[count];
+		for (int i = 0; i < count; i++)
+		{
+			float a = (float)i / (float)count * 2.0f * Mathf.PI;
+			points[i] = new Vector3(Mathf.Sin(a), 0.0f, Mathf.Cos(a));
+		}
+
+		lineRenderer.SetPositions(points);
+	}
+
+	void Update()
+	{
+		// Change the line width based on the distance from the camera to the closest point on the line.
+		LineRenderer lineRenderer = GetComponent<LineRenderer>();
+
+
+		//Vector3.Distance()
+	}
 }
