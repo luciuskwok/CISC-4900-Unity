6128a84fe57c45d72f637754119cd1941f9a3d09
Lucius Kwok <lk@felttip.com>
2025-04-24 17:48:59 -0400

    Wrote code to plot an ellipse
    Used the polar form of the equation for an ellipse, along with a rotation for the periapsis.

---
+++ OMS/Assets/Scripts/OrbitPlot.cs
@@ -0,0 +1,57 @@
+using UnityEngine;
+using System.Collections;
+
+public class OrbitPlot : MonoBehaviour
+{
+	public float semiMajorAxis;
+	public float eccentricity; // 0 = circle; 1 = parabola
+	public float longitudeOfPeriapsis; // in degrees
+
+	// Note: the coordinate system that is conventionally used for orbital mechanics has the ecliptic on the x-y plane, and positive z is towards the North Pole Star. The positive x axis is the direction of the Sun as seen from the Earth at the (spring) vernal equinox. This means that the Earth is at longitude 0 at the autumnal equinox, and at 180 at the spring equinox.
+	// Unity uses the convention that the x-z plane is horizontal, and positive y points up. So the y and z axes are swapped. 
+	// As for the x axis, that can be arbitrarily chosen as long as it is consistent throughout the solar system for this project. 
+
+	private LineRenderer lineRenderer;
+	private int pointCount = 90;
+
+	void Start()
+	{
+		lineRenderer = GetComponent<LineRenderer>();
+		UpdatePoints();
+	}
+
+	public void UpdatePoints() {
+		lineRenderer.positionCount = pointCount;
+		var points = GetOrbitPoints(pointCount);
+		lineRenderer.SetPositions(points);
+	}
+
+	Vector3[] GetOrbitPoints(int count) {
+		var points = new Vector3[count];
+
+		// First, calculate the olar form of ellipse relative to focus, then rotate it so its periapsis is at the specified longitude, and finally convert to cartesian coordinates.
+		// Pre-calculate the semi-latus rectum (l)
+		float semiLactusRectum = semiMajorAxis * (1.0f + eccentricity * eccentricity);
+		// Pre-convert the longitude of periapsis from degrees to radians
+		float longOfPeriapsisRadians = Mathf.Deg2Rad * longitudeOfPeriapsis;
+		for (int i = 0; i < count; i++) {
+			// Calculate the orbit from -180 to 100 degrees
+			float theta = ((float)i / (float)count * 360.0f - 180.0f) * Mathf.Deg2Rad;
+			// This version of the equation has the reference direction theta = 0 pointing away from the center of the ellipse, so that the zero angle is at the periapsis of the orbit.
+			float r = semiLactusRectum / ( 1.0f - eccentricity * Mathf.Cos(theta));
+			// Rotate by the longitudde of periapsis plus 180 degrees so that the zero longitude is at the positive x axis.
+			float a = theta + longOfPeriapsisRadians + Mathf.PI;
+			// Convert from polar to cartesian coordinates.
+			float x = Mathf.Cos(a) * r;
+			float z = Mathf.Sin(a) * r;
+			points[i] = new Vector3(x, 0, z);
+		}
+
+		return points;
+	}
+
+	void Update()
+	{
+		
+	}
+}
