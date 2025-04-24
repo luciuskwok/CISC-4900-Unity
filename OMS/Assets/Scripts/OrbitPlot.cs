50c248dc38fc6cd8f81646fe5951e76c2201f1dc
Lucius Kwok <lk@felttip.com>
2025-04-24 19:16:25 -0400

    Added color gradient animation
    Orbit lines now have an animation that rotates in the direction of the orbit.

--- OMS/Assets/Scripts/OrbitPlot.cs
+++ OMS/Assets/Scripts/OrbitPlot.cs
@@ -1,11 +1,14 @@
 using UnityEngine;
 using System.Collections;
+using Unity.VisualScripting.Antlr3.Runtime;
+using System;
 
 public class OrbitPlot : MonoBehaviour
 {
 	public float semiMajorAxis;
 	public float eccentricity; // 0 = circle; 1 = parabola
 	public float longitudeOfPeriapsis; // in degrees
+	public Color color;
 
 	// Note: the coordinate system that is conventionally used for orbital mechanics has the ecliptic on the x-y plane, and positive z is towards the North Pole Star. The positive x axis is the direction of the Sun as seen from the Earth at the (spring) vernal equinox. This means that the Earth is at longitude 0 at the autumnal equinox, and at 180 at the spring equinox.
 	// Unity uses the convention that the x-z plane is horizontal, and positive y points up. So the y and z axes are swapped. 
@@ -13,11 +16,19 @@ public class OrbitPlot : MonoBehaviour
 
 	private LineRenderer lineRenderer;
 	private int pointCount = 90;
+	private double gradientAnimationTime = 4.0f; // seconds
 
 	void Start()
 	{
 		lineRenderer = GetComponent<LineRenderer>();
 		UpdatePoints();
+	}
+
+	void Update()
+	{
+		double x = (Time.timeSinceLevelLoadAsDouble / gradientAnimationTime) % 1.0f;
+		int step = (int)Math.Round(x * pointCount);
+		UpdateColors(step);
 	}
 
 	public void UpdatePoints() {
@@ -38,7 +49,7 @@ public class OrbitPlot : MonoBehaviour
 			float theta = ((float)i / (float)count * 360.0f - 180.0f) * Mathf.Deg2Rad;
 			// This version of the equation has the reference direction theta = 0 pointing away from the center of the ellipse, so that the zero angle is at the periapsis of the orbit.
 			float r = radiusWithTrueAnomaly(theta);
-			// Rotate by the longitudde of periapsis plus 180 degrees so that the zero longitude is at the positive x axis.
+			// Rotate by the longitudde of periapsis, which is locade at theta = 0, relative to the ecliptic coordinate system, where longitude = 0 is at the positive x axis.
 			float a = theta + longOfPeriapsisRadians;
 			// Convert from polar to cartesian coordinates.
 			float x = Mathf.Cos(a) * r;
@@ -57,8 +68,44 @@ public class OrbitPlot : MonoBehaviour
 		return semiLactusRectum / ( 1.0f + eccentricity * Mathf.Cos(theta));
 	}
 
-	void Update()
-	{
+	float trueAnomayWithEccentricAnomaly(float e) {
 		
+		return 0;
+	}
+
+	void UpdateColors(int step) {
+		// Given step as the index of a point, create a gradient.
+		Gradient gradient = new Gradient();
+
+		// Calculate the locations and alpha values for a fade
+		float countf = (float)(pointCount + 1);
+		float x1 = (float)step / countf;
+		float x2 = (float)(step + 1) / countf;
+		float a1 = 1.0f;
+		float a2 = 0.25f;
+		float a0 = a2 + (a1 - a2) * (1.0f - x1);
+		float a3 = a2 + (a1 - a2) * (1.0f - x2);
+
+		if (x2 > 1.0f) {
+			x2 = 0.0f;
+			a3 = a2;
+		}
+
+
+		gradient.SetKeys(
+			// Use the same color for entire line
+			new GradientColorKey[] { 
+				new GradientColorKey(color, 0.0f), 
+				new GradientColorKey(color, 1.0f) },
+			// Vary the alpha 
+			new GradientAlphaKey[] { 
+				new GradientAlphaKey(a0, 0.0f),
+				new GradientAlphaKey(a1, x1),
+				new GradientAlphaKey(a2, x2),
+				new GradientAlphaKey(a3, 1.0f),
+			}
+		);
+
+		lineRenderer.colorGradient = gradient;
 	}
 }
