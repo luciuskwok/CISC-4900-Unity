e34072ab3501c5d9d258c52e03f71b24dd74dc88
Lucius Kwok <lk@felttip.com>
2025-05-03 03:05:08 -0400

    Started script for rocket control

---
+++ OMS/Assets/Scripts/RocketController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class RocketController : MonoBehaviour
+{
+	public double dryMass; // kg
+	public double wetMass; // kg
+	public double consumptionRate; // kg/second
+	public double thrust; // kN
+
+	void Start()
+	{
+		
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		
+	}
+}
