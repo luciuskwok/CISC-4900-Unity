92f72d861fb0222a50dad73df98c3175303d6820
Lucius Kwok <lk@felttip.com>
2025-05-03 03:38:41 -0400

    Added physics calculations for launch
    The calculations seem off. Also, reorganized the reference images.

--- OMS/Assets/Scripts/RocketController.cs
+++ OMS/Assets/Scripts/RocketController.cs
@@ -6,15 +6,49 @@ public class RocketController : MonoBehaviour
 	public double wetMass; // kg
 	public double consumptionRate; // kg/second
 	public double thrust; // kN
+	public double countdown; // seconds to wait before release
+
+	private Vector3 velocity;
+	private double gravity = 9.81; // m/s^2 
 
 	void Start()
 	{
-		
+		velocity = new Vector3();
 	}
 
-	// Update is called once per frame
 	void Update()
 	{
-		
+		if (countdown > 0.0) {
+			countdown -= Time.deltaTime;
+			if (countdown <= 0.0) {
+				countdown = 0.0;
+			} else {
+				return;
+			}
+		}
+
+
+		// force = mass * acceleration
+		// acceleration = force / mass
+		double totalMass = dryMass + wetMass;
+		double rocketAcceleration = 0.0;
+		if (wetMass > 0.0) {
+			rocketAcceleration = thrust / totalMass;
+			wetMass -= consumptionRate * Time.deltaTime;
+			if (wetMass < 0.0) wetMass = 0.0;
+		}
+
+		// Apply rocket acceleration to rotated Y-axis to velocity vector
+		Vector3 forwardVec = transform.TransformDirection(Vector3.up) * (float)rocketAcceleration;
+
+		// Apply gravity
+		Vector3 gravityVec = Vector3.down * (float)gravity;
+
+		// Add both accelerations to rocket;
+		velocity += (forwardVec + gravityVec) * Time.deltaTime;
+
+		// Translate according to velocity;
+		transform.Translate(velocity * Time.deltaTime);
+
 	}
 }
