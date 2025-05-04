5c67063789faf9fd420755363ba155a1e3917de9
Lucius Kwok <lk@felttip.com>
2025-05-03 20:43:23 -0400

    Added code to update readouts

--- OMS/Assets/Scripts/RocketController.cs
+++ OMS/Assets/Scripts/RocketController.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class RocketController : MonoBehaviour
@@ -7,7 +8,11 @@ public class RocketController : MonoBehaviour
 	public double consumptionRate; // kg/second
 	public double thrust; // kN
 
-	public double countdown; // seconds to wait before release
+	public double missionTime; // seconds to wait before release
+
+	public TMP_Text speedReadout; 
+	public TMP_Text altitudeReadout;
+	public TMP_Text timeReadout;
 
 	private bool isHeldDown = true; // when true, gravity and thrust are not applied
 	private Vector3 velocity = new();
@@ -15,43 +20,66 @@ public class RocketController : MonoBehaviour
 
 	void Start()
 	{
-		
+		UpdateText();
 	}
 
 	void Update()
 	{
-		if (countdown > 0.0) {
-			countdown -= Time.deltaTime;
-			if (countdown <= 0.0) {
-				countdown = 0.0;
-			} else {
-				return;
+		// Update mission time and release hold downs
+		double oldTIme = missionTime;
+		missionTime += Time.deltaTime;
+		if (missionTime >= 0.0) isHeldDown = false;
+
+		// Apply forces if hold downs are released
+		if (!isHeldDown) {
+			// force = mass * acceleration
+			// acceleration = force / mass
+			double totalMass = dryMass + wetMass;
+			double rocketAcceleration = 0.0;
+			if (wetMass > 0.0) {
+				rocketAcceleration = thrust / totalMass * 1000.0;
+				wetMass -= consumptionRate * Time.deltaTime;
+				if (wetMass < 0.0) wetMass = 0.0;
 			}
+
+			// Apply rocket acceleration to rotated Y-axis to velocity vector
+			Vector3 forwardVec = transform.TransformDirection(Vector3.up) * (float)rocketAcceleration;
+
+			// Apply gravity
+			Vector3 gravityVec = Vector3.down * (float)gravity;
+
+			// Add both accelerations to rocket;
+			velocity += (forwardVec + gravityVec) * Time.deltaTime;
+
+			// Translate according to velocity;
+			transform.Translate(velocity * Time.deltaTime);
+		}
+		
+		UpdateText();
+	}
+
+	void UpdateText() {
+		// Speed
+		double speed = velocity.magnitude;
+		speedReadout.text = speed.ToString("#,##0") + " m/s";
+
+		// Altitude
+		Vector3 position;
+		Quaternion rotation; 
+		transform.GetPositionAndRotation(out position, out rotation);
+		double altitude = position.y;
+		if (altitude < 2000.0) {
+			altitudeReadout.text = altitude.ToString("#,##0") + " m";
+		} else {
+			altitudeReadout.text = (altitude/1000.0).ToString("#,##0") + " km";
 		}
 
-		if (isHeldDown) return;
-
-		// force = mass * acceleration
-		// acceleration = force / mass
-		double totalMass = dryMass + wetMass;
-		double rocketAcceleration = 0.0;
-		if (wetMass > 0.0) {
-			rocketAcceleration = thrust / totalMass * 1000.0;
-			wetMass -= consumptionRate * Time.deltaTime;
-			if (wetMass < 0.0) wetMass = 0.0;
+		// Time
+		if (missionTime < 0.0) {
+			double t = -missionTime;
+			timeReadout.text = "T-" + StringUtil.FormatTimeWithColons(t);
+		} else {
+			timeReadout.text = "T+" + StringUtil.FormatTimeWithColons(missionTime);
 		}
-
-		// Apply rocket acceleration to rotated Y-axis to velocity vector
-		Vector3 forwardVec = transform.TransformDirection(Vector3.up) * (float)rocketAcceleration;
-
-		// Apply gravity
-		Vector3 gravityVec = Vector3.down * (float)gravity;
-
-		// Add both accelerations to rocket;
-		velocity += (forwardVec + gravityVec) * Time.deltaTime;
-
-		// Translate according to velocity;
-		transform.Translate(velocity * Time.deltaTime);
-
 	}
 }
