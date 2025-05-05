f4c3dd0f4b4b9e59c595b616f5c4d704bad5f7ba
Lucius Kwok <lk@felttip.com>
2025-05-05 16:40:44 -0400

    Hide nodes behind camera

--- OMS/Assets/Scripts/UI Widgets/UINode.cs
+++ OMS/Assets/Scripts/UI Widgets/UINode.cs
@@ -12,14 +12,19 @@ public class UINode : MonoBehaviour
 	public void UpdateCanvasPosition() {
 		// Convert to 2d 
 		Vector3 point = Camera.main.WorldToViewportPoint(m_WorldPosition);
-		// Scale to canvas size
-		GameObject canvas = GameObject.Find("Canvas");
-		RectTransform canvasRT = canvas.GetComponent<RectTransform>();
-		point.x *= canvasRT.rect.width;
-		point.y *= canvasRT.rect.height;
-		// Move node in UI
-		RectTransform nodeRT = gameObject.GetComponent<RectTransform>();
-		nodeRT.anchoredPosition = point;
-
+		// Check if point is in front of camera
+		if (point.z > 0.0) {
+			gameObject.SetActive (true);
+			// Scale to canvas size
+			GameObject canvas = GameObject.Find("Canvas");
+			RectTransform canvasRT = canvas.GetComponent<RectTransform>();
+			point.x *= canvasRT.rect.width;
+			point.y *= canvasRT.rect.height;
+			// Move node in UI
+			RectTransform nodeRT = gameObject.GetComponent<RectTransform>();
+			nodeRT.anchoredPosition = point;
+		} else {
+			gameObject.SetActive (false);
+		}
 	}
 }
