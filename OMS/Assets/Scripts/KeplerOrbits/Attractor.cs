c1a0eb25d2f95a365ae2970c0a054ffad50d3901
Lucius Kwok <lk@felttip.com>
2025-05-05 18:55:24 -0400

    Moved planets to correct positions
    Also changed the orbit plots from animated to showing the gradient ending at current position of planet.

--- OMS/Assets/Scripts/KeplerOrbits/Attractor.cs
+++ OMS/Assets/Scripts/KeplerOrbits/Attractor.cs
@@ -9,18 +9,17 @@ public class Attractor {
 	public double mass; // kg
 	public double radius; // km
 	public double influence; // km, for sphere of influence
+	public Vector3d position; // km
 
-	private static Attractor m_Earth;
+	public static Attractor Sun {
+		get { 
+			return new Attractor(1.9885e30 /* kg mass */, 1.3914e6 /* km radius */, 1.0e12 /* km influence */ );
+		}
+	}
+
 	public static Attractor Earth {
 		get { 
-			if (m_Earth == null) {
-				m_Earth = new Attractor(
-					5.9722e24, // kg mass
-					6378.0, // km radius
-					9.29e5 // km influence
-				);
-			}
-			return m_Earth;
+			return new Attractor(5.9722e24 /* kg mass */, 6378.0 /* km radius */, 9.29e5 /* km influence */ );
 		}
 	}
 
@@ -28,6 +27,7 @@ public class Attractor {
 		this.mass = mass;
 		this.radius = radius;
 		this.influence = influence;
+		position = Vector3d.zero;
 	}
 
 }
--- OMS/Assets/Scripts/SolarSystemController.cs
+++ OMS/Assets/Scripts/SolarSystemController.cs
@@ -1,6 +1,4 @@
-using System.Linq;
-using Unity.VisualScripting;
-using UnityEditor;
+using TMPro;
 using UnityEngine;
 
 public class SolarSystemController : MonoBehaviour
@@ -8,45 +6,40 @@ public class SolarSystemController : MonoBehaviour
 	public GameObject orbitPlotPrefab;
 	public GameObject orbitsParent;
 	public GameObject nodesParent;
+	public GameObject targetsParent;
 
-	private Attractor m_Sun;
-	private double m_AnimationTimeScale;
+	// UI elements
+	public TMP_Text targetReadout;
+
 	private int m_PlanetCount = 8;
 	private GameObject[] m_OrbitPlots;
-	private GameObject[] m_Nodes;
 	
 
 	void Start()
 	{
 		// Animation time scale: 1 year = 15 seconds
-		m_AnimationTimeScale = 15.0 * 60.0 * 24.0 * 365.0;
+		//m_AnimationTimeScale = 60.0/15.0 * 60.0 * 24.0 * 365.0;
 
-		// Sun      mass, kg   radius    influence, km
-		m_Sun = new(1.9885e30, 1.3914e6, 1.0e12);
-
-		// Nodes
-		int count = nodesParent.transform.childCount;
-		m_Nodes = new GameObject[count];
-		for (int index = 0; index < count; index++) {
-			m_Nodes[index] = nodesParent.transform.GetChild(index).gameObject;
-		}
-
+		// Attractors
+		Attractor sun = Attractor.Sun;
+		Attractor earth = Attractor.Earth;		
+	
 		// Orbit Plots
 		m_OrbitPlots = new GameObject[m_PlanetCount];
 		int i = 0;
 		// Planets                               hue     ecc        SMA   incl     AOP      AN    MLAE
-		m_OrbitPlots[i++] = SpawnOrbit("Mercury",  0, 0.2056, 5.79091e7, 7.006,  29.12,  48.34, 252.25);
-		m_OrbitPlots[i++] = SpawnOrbit("Venus",  290, 0.0068, 1.08209e8, 3.398,  54.88,  76.67, 181.98);
-		m_OrbitPlots[i++] = SpawnOrbit("Earth",  190, 0.0167, 1.49598e8, 0.000, 114.21,   0.00, 100.47);
-		m_OrbitPlots[i++] = SpawnOrbit("Mars",    20, 0.0934, 2.27940e8, 1.852, 286.50,  49.71, 355.43);
-		m_OrbitPlots[i++] = SpawnOrbit("Jupiter", 40, 0.0489, 7.78478e8, 1.299, 273.87, 100.29,  34.33);
-		m_OrbitPlots[i++] = SpawnOrbit("Saturn",  60, 0.0565, 1.43354e9, 2.494, 339.39, 113.64,  50.08);
-		m_OrbitPlots[i++] = SpawnOrbit("Uranus", 160, 0.0472, 2.87097e9, 0.077,  97.00,  73.96, 314.20);
-		m_OrbitPlots[i++] = SpawnOrbit("Neptune",210, 0.0087, 4.49841e9, 1.770, 273.19, 131.79, 304.22);
+		m_OrbitPlots[i++] = SpawnOrbit("Mercury",  0, 0.2056, 5.79091e7, 7.006,  29.12,  48.34, 252.25, sun);
+		m_OrbitPlots[i++] = SpawnOrbit("Venus",  290, 0.0068, 1.08209e8, 3.398,  54.88,  76.67, 181.98, sun);
+		m_OrbitPlots[i++] = SpawnOrbit("Earth",  190, 0.0167, 1.49598e8, 0.000, 114.21,   0.00, 100.47, sun);
+		m_OrbitPlots[i++] = SpawnOrbit("Mars",    20, 0.0934, 2.27940e8, 1.852, 286.50,  49.71, 355.43, sun);
+		m_OrbitPlots[i++] = SpawnOrbit("Jupiter", 40, 0.0489, 7.78478e8, 1.299, 273.87, 100.29,  34.33, sun);
+		m_OrbitPlots[i++] = SpawnOrbit("Saturn",  60, 0.0565, 1.43354e9, 2.494, 339.39, 113.64,  50.08, sun);
+		m_OrbitPlots[i++] = SpawnOrbit("Uranus", 160, 0.0472, 2.87097e9, 0.077,  97.00,  73.96, 314.20, sun);
+		m_OrbitPlots[i++] = SpawnOrbit("Neptune",210, 0.0087, 4.49841e9, 1.770, 273.19, 131.79, 304.22, sun);
 		
 	}
 
-	GameObject SpawnOrbit(string name, float hue, double eccentricity, double semiMajorAxis, double inclinationDeg, double argOfPerifocusDeg, double ascendingNodeDeg, double meanLongAtEpoch) 
+	GameObject SpawnOrbit(string name, float hue, double eccentricity, double semiMajorAxis, double inclinationDeg, double argOfPerifocusDeg, double ascendingNodeDeg, double meanLongAtEpoch, Attractor attractor) 
 	{
 		// Create the game object from a prefab
 		GameObject orbit = Instantiate(orbitPlotPrefab, Vector3.zero, Quaternion.identity);
@@ -57,9 +50,9 @@ public class SolarSystemController : MonoBehaviour
 
 		// Set up the orbital elements
 		OrbitPlot plot = orbit.GetComponent<OrbitPlot>();
-		plot.animationTimeScale = m_AnimationTimeScale;
+		plot.animate = false;
 		plot.color = Color.HSVToRGB(hue / 360.0f, 0.925f, 0.925f);
-		plot.attractor = m_Sun;
+		plot.attractor = attractor;
 		plot.SetOrbitalElements(eccentricity, 
 			semiMajorAxis, 
 			inclinationDeg * Kepler.Deg2Rad, 
@@ -92,9 +85,19 @@ public class SolarSystemController : MonoBehaviour
 			OrbitPlot plot = m_OrbitPlots[i].GetComponent<OrbitPlot>();
 			Vector3 position = plot.GetWorldPositionAtTime(atTime);
 
-			if (i < m_Nodes.Length) {
-				UINode node = m_Nodes[i].GetComponent<UINode>();
+			// Set orbit plot color gradient
+			plot.UpdateGradientWithTime(atTime);
+
+			// Position node on 2D canvas
+			if (i < nodesParent.transform.childCount) {
+				UINode node = nodesParent.transform.GetChild(i).gameObject.GetComponent<UINode>();
 				node.SetWorldPosition(position);
+			}
+
+			// Position planets in 3d world space
+			if (i < targetsParent.transform.childCount) {
+				Transform target = targetsParent.transform.GetChild(i);
+				target.localPosition = target.worldToLocalMatrix * position;
 			}
 		}
 	}
