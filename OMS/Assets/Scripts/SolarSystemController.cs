c1f7078af002b29eb769f649a3abbd370277cb20
Lucius Kwok <lk@felttip.com>
2025-05-06 11:22:31 -0400

    Moved orbit plot creation out of code
    This allows for more customization of visual settings per orbit

--- OMS/Assets/Scripts/SolarSystemController.cs
+++ OMS/Assets/Scripts/SolarSystemController.cs
@@ -20,8 +20,6 @@ public class SolarSystemController : MonoBehaviour
 	public TMP_Text distanceReadout;
 	
 
-	private int m_PlanetCount = 9;
-	private GameObject[] m_OrbitPlots;
 	private Attractor m_Sun;
 	private Attractor m_Earth;
 	private int m_TargetIndex;
@@ -38,18 +36,15 @@ public class SolarSystemController : MonoBehaviour
 		m_Earth = Attractor.Earth;		
 	
 		// Orbit Plots
-		m_OrbitPlots = new GameObject[m_PlanetCount];
-		int i = 0;
-		// Planets                     hue     ecc        SMA   incl     AOP      AN    MLAE
-		m_OrbitPlots[i++] = SpawnOrbit(  0, 0.2056, 5.79091e7, 7.006,  29.12,  48.34, 252.25, m_Sun); // Mercury
-		m_OrbitPlots[i++] = SpawnOrbit(290, 0.0068, 1.08209e8, 3.398,  54.88,  76.67, 181.98, m_Sun); // Venus
-		m_OrbitPlots[i++] = SpawnOrbit(190, 0.0167, 1.49598e8, 0.000, 114.21,   0.00, 100.47, m_Sun); // Earth
-		m_OrbitPlots[i++] = SpawnOrbit(120, 0.0055, 3.84748e5, 5.100,      0,   0.00,      0, m_Earth); // Moon
-		m_OrbitPlots[i++] = SpawnOrbit( 20, 0.0934, 2.27940e8, 1.852, 286.50,  49.71, 355.43, m_Sun); // Mars
-		m_OrbitPlots[i++] = SpawnOrbit( 40, 0.0489, 7.78478e8, 1.299, 273.87, 100.29,  34.33, m_Sun); // Jupiter
-		m_OrbitPlots[i++] = SpawnOrbit( 60, 0.0565, 1.43354e9, 2.494, 339.39, 113.64,  50.08, m_Sun); // Saturn
-		m_OrbitPlots[i++] = SpawnOrbit(160, 0.0472, 2.87097e9, 0.077,  97.00,  73.96, 314.20, m_Sun); // Uranus
-		m_OrbitPlots[i++] = SpawnOrbit(210, 0.0087, 4.49841e9, 1.770, 273.19, 131.79, 304.22, m_Sun); // Neptune
+		SetUpOrbit(0, 0.2056, 5.79091e7, 7.006,  29.12,  48.34, 252.25, m_Sun); // Mercury
+		SetUpOrbit(1, 0.0068, 1.08209e8, 3.398,  54.88,  76.67, 181.98, m_Sun); // Venus
+		SetUpOrbit(2, 0.0167, 1.49598e8, 0.000, 114.21,   0.00, 100.47, m_Sun); // Earth
+		SetUpOrbit(3, 0.0055, 3.84748e5, 5.100,      0,   0.00,      0, m_Earth); // Moon
+		SetUpOrbit(4, 0.0934, 2.27940e8, 1.852, 286.50,  49.71, 355.43, m_Sun); // Mars
+		SetUpOrbit(5, 0.0489, 7.78478e8, 1.299, 273.87, 100.29,  34.33, m_Sun); // Jupiter
+		SetUpOrbit(6, 0.0565, 1.43354e9, 2.494, 339.39, 113.64,  50.08, m_Sun); // Saturn
+		SetUpOrbit(7, 0.0472, 2.87097e9, 0.077,  97.00,  73.96, 314.20, m_Sun); // Uranus
+		SetUpOrbit(8, 0.0087, 4.49841e9, 1.770, 273.19, 131.79, 304.22, m_Sun); // Neptune
 		
 		// Move planets into position
 		UpdatePlanetPositions(0);
@@ -58,19 +53,13 @@ public class SolarSystemController : MonoBehaviour
 		SetCameraTargetAtIndex(2); // Start with Earth
 	}
 
-	GameObject SpawnOrbit(float hue, double eccentricity, double semiMajorAxis, double inclinationDeg, double argOfPerifocusDeg, double ascendingNodeDeg, double meanLongAtEpoch, Attractor attractor) 
+	GameObject SetUpOrbit(int index, double eccentricity, double semiMajorAxis, double inclinationDeg, double argOfPerifocusDeg, double ascendingNodeDeg, double meanLongAtEpoch, Attractor attractor) 
 	{
-		// Create the game object from a prefab
-		GameObject orbit = Instantiate(orbitPlotPrefab, Vector3.zero, Quaternion.identity);
-		orbit.transform.parent = orbitsParent.transform;
-		// Reset the transform because assigning a parent causes the child's transform to change
-		orbit.transform.SetLocalPositionAndRotation(Vector3.zero, Quaternion.identity);
-		orbit.transform.localScale = Vector3.one;
+		// Get the orbit plot
+		GameObject orbit = orbitsParent.transform.GetChild(index).gameObject;
 
 		// Set up the orbital elements
 		OrbitPlot plot = orbit.GetComponent<OrbitPlot>();
-		plot.animate = false;
-		plot.color = Color.HSVToRGB(hue / 360.0f, 0.925f, 0.925f);
 		plot.attractor = attractor;
 		plot.SetOrbitalElements(eccentricity, 
 			semiMajorAxis, 
@@ -98,14 +87,15 @@ public class SolarSystemController : MonoBehaviour
 	}
 
 	void UpdatePlanetPositions(double atTime) {
+		int count = orbitsParent.transform.childCount;
 
 		// Update gradient, node, and bodiesd of all planets and moons
-		for (int i = 0; i < m_PlanetCount; i++) {
-			OrbitPlot plot = m_OrbitPlots[i].GetComponent<OrbitPlot>();
+		for (int i = 0; i < count; i++) {
+			OrbitPlot plot = orbitsParent.transform.GetChild(i).gameObject.GetComponent<OrbitPlot>();
 			Vector3 position = plot.GetWorldPositionAtTime(atTime);
 
 			if (i == 2) { // Special case for Moon-Earth system
-				GameObject moon = m_OrbitPlots[3];
+				GameObject moon = orbitsParent.transform.GetChild(3).gameObject;
 				moon.transform.localPosition = moon.transform.worldToLocalMatrix * position;
 			}
 
