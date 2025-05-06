e0f194302d4d07493a6338925a19c0da05392ce9
Lucius Kwok <lk@felttip.com>
2025-05-05 21:13:49 -0400

    Implemented changing targets

--- OMS/Assets/Scripts/SolarSystemController.cs
+++ OMS/Assets/Scripts/SolarSystemController.cs
@@ -10,8 +10,8 @@ public class SolarSystemController : MonoBehaviour
 
 	// Camera
 	public GameObject cameraTarget;
-	private float distanceMax = 8.8e10f; // km
-	private float distanceMin = 1.0f; // varies based on target
+	private float m_DistanceMax = 8.8e10f; // km
+	private float m_DistanceMin = 1.0f; // varies based on target
 	private float m_MousePanTiltSpeed = 0.1f;
 	private float m_KeyboardPanTiltSpeed = 30.0f;
 
@@ -38,19 +38,24 @@ public class SolarSystemController : MonoBehaviour
 		// Orbit Plots
 		m_OrbitPlots = new GameObject[m_PlanetCount];
 		int i = 0;
-		// Planets                               hue     ecc        SMA   incl     AOP      AN    MLAE
-		m_OrbitPlots[i++] = SpawnOrbit("Mercury",  0, 0.2056, 5.79091e7, 7.006,  29.12,  48.34, 252.25, sun);
-		m_OrbitPlots[i++] = SpawnOrbit("Venus",  290, 0.0068, 1.08209e8, 3.398,  54.88,  76.67, 181.98, sun);
-		m_OrbitPlots[i++] = SpawnOrbit("Earth",  190, 0.0167, 1.49598e8, 0.000, 114.21,   0.00, 100.47, sun);
-		m_OrbitPlots[i++] = SpawnOrbit("Mars",    20, 0.0934, 2.27940e8, 1.852, 286.50,  49.71, 355.43, sun);
-		m_OrbitPlots[i++] = SpawnOrbit("Jupiter", 40, 0.0489, 7.78478e8, 1.299, 273.87, 100.29,  34.33, sun);
-		m_OrbitPlots[i++] = SpawnOrbit("Saturn",  60, 0.0565, 1.43354e9, 2.494, 339.39, 113.64,  50.08, sun);
-		m_OrbitPlots[i++] = SpawnOrbit("Uranus", 160, 0.0472, 2.87097e9, 0.077,  97.00,  73.96, 314.20, sun);
-		m_OrbitPlots[i++] = SpawnOrbit("Neptune",210, 0.0087, 4.49841e9, 1.770, 273.19, 131.79, 304.22, sun);
+		// Planets                     hue     ecc        SMA   incl     AOP      AN    MLAE
+		m_OrbitPlots[i++] = SpawnOrbit(  0, 0.2056, 5.79091e7, 7.006,  29.12,  48.34, 252.25, sun); // Mercury
+		m_OrbitPlots[i++] = SpawnOrbit(290, 0.0068, 1.08209e8, 3.398,  54.88,  76.67, 181.98, sun); // Venus
+		m_OrbitPlots[i++] = SpawnOrbit(190, 0.0167, 1.49598e8, 0.000, 114.21,   0.00, 100.47, sun); // Earth
+		m_OrbitPlots[i++] = SpawnOrbit( 20, 0.0934, 2.27940e8, 1.852, 286.50,  49.71, 355.43, sun); // Mars
+		m_OrbitPlots[i++] = SpawnOrbit( 40, 0.0489, 7.78478e8, 1.299, 273.87, 100.29,  34.33, sun); // Jupiter
+		m_OrbitPlots[i++] = SpawnOrbit( 60, 0.0565, 1.43354e9, 2.494, 339.39, 113.64,  50.08, sun); // Saturn
+		m_OrbitPlots[i++] = SpawnOrbit(160, 0.0472, 2.87097e9, 0.077,  97.00,  73.96, 314.20, sun); // Uranus
+		m_OrbitPlots[i++] = SpawnOrbit(210, 0.0087, 4.49841e9, 1.770, 273.19, 131.79, 304.22, sun); // Neptune
 		
+		// Move planets into position
+		UpdatePlanetPositions(0);
+		
+		// Target
+		SetCameraTargetAtIndex(0);
 	}
 
-	GameObject SpawnOrbit(string name, float hue, double eccentricity, double semiMajorAxis, double inclinationDeg, double argOfPerifocusDeg, double ascendingNodeDeg, double meanLongAtEpoch, Attractor attractor) 
+	GameObject SpawnOrbit(float hue, double eccentricity, double semiMajorAxis, double inclinationDeg, double argOfPerifocusDeg, double ascendingNodeDeg, double meanLongAtEpoch, Attractor attractor) 
 	{
 		// Create the game object from a prefab
 		GameObject orbit = Instantiate(orbitPlotPrefab, Vector3.zero, Quaternion.identity);
@@ -80,8 +85,6 @@ public class SolarSystemController : MonoBehaviour
 		const float lineWidth = 20.0f;
 		lineRenderer.startWidth = lineWidth;
 		lineRenderer.endWidth = lineWidth;
-
-		// TODO: save name somewhere
 		
 		return orbit;
 	}
@@ -139,8 +142,8 @@ public class SolarSystemController : MonoBehaviour
 			m_LastMousePosition = Input.mousePosition;
 		}
 
-		// Keyboard
-		// -- Arrow Keys: pan & tilt camera
+		// ## Keyboard
+		// Pan/Tilt
 		float movement = Time.deltaTime * m_KeyboardPanTiltSpeed * shiftSpeed; 
 		if (Input.GetKey(KeyCode.UpArrow)) {
 			PanTiltCamera(0.0f, movement);
@@ -151,11 +154,21 @@ public class SolarSystemController : MonoBehaviour
 		} else if (Input.GetKey(KeyCode.RightArrow)) {
 			PanTiltCamera(-movement, 0.0f);
 		}
-		// -- Plus ('=' or '+') / Minus ('-' or '_'): dolly in/out
+		// Dolly
 		else if (Input.GetKey(KeyCode.Minus)) {
 			DollyCamera(-movement * 0.2f);
 		} else if (Input.GetKey(KeyCode.Equals)) {
 			DollyCamera(movement * 0.2f);
+		}
+
+		// Switching Targets
+		if (Input.GetKeyDown(KeyCode.LeftBracket))
+		{
+			GoToPreviousTarget();
+		}
+		else if (Input.GetKeyDown(KeyCode.RightBracket))
+		{
+			GoToNextTarget();
 		}
 
 
@@ -171,8 +184,8 @@ public class SolarSystemController : MonoBehaviour
 		distance = Mathf.Pow(10.0f, Mathf.Log10(distance) - delta * 0.2f);
 
 		// Clamp the values to min and max
-		distance = (distance < distanceMin) ? distanceMin : distance;
-		distance = (distance > distanceMax) ? distanceMax : distance;
+		distance = (distance < m_DistanceMin) ? m_DistanceMin : distance;
+		distance = (distance > m_DistanceMax) ? m_DistanceMax : distance;
 
 		// Set new camera position
 		camera.transform.localPosition = new Vector3(0, 0, -distance);
@@ -195,6 +208,39 @@ public class SolarSystemController : MonoBehaviour
 		if (tilt > tiltLimit) tilt = tiltLimit;
 
 		cameraTarget.transform.localEulerAngles = new Vector3(tilt, pan, 0);
+	}
+
+	void GoToNextTarget() {
+		SetCameraTargetAtIndex(m_TargetIndex + 1);
+	}
+
+	void GoToPreviousTarget() {
+		SetCameraTargetAtIndex(m_TargetIndex - 1);
+	}
+
+	void SetCameraTargetAtIndex(int index)
+	{
+		int count = targetsParent.transform.childCount;
+		index %= count;
+		if (index < 0) index += count;
+		m_TargetIndex = index;
+		GameObject newTarget = targetsParent.transform.GetChild(index).gameObject;
+
+		// Update minimum distance
+		Transform planetBody = newTarget.transform.GetChild(0);
+		m_DistanceMin = planetBody.localScale.x * 1.5f;
+
+		// Move camera if needed
+		Camera camera = Camera.main;
+		if (-camera.transform.localPosition.z < m_DistanceMin) {
+			camera.transform.localPosition = new Vector3(0, 0, -m_DistanceMin);
+		}
+
+		// Set new target position
+		cameraTarget.transform.position = newTarget.transform.position;
+
+		// Set target readout
+		targetReadout.text = newTarget.name;
 	}
 
 	void UpdateReadouts() {
